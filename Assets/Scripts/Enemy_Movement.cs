542f7a38e9aa18b0541dfabbc658093c898d420f
Mel Rodrigues <melrodriguesgames@gmail.com>
2021-01-18 12:59:37 -0800

    Enemy movement.

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start(){
+
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update() {
+
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.1f){
+            
+            GetNextWaypoint();
+        }
+        
+    }
+
+    private void GetNextWaypoint(){
+
+        if(waypointIndex >= Waypoints.waypoints.Length - 1){
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+}
--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static Transform[] waypoints;
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Awake(){
         
+        waypoints = new Transform[transform.childCount];
+        for (int i = 0; i < waypoints.Length; i++){
+            waypoints[i] = transform.GetChild(i);
+        }
     }
 }

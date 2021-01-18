7a621f18f3785a817716a65ac48b7a3431b1809b
Mel Rodrigues <melrodriguesgames@gmail.com>
2021-01-18 13:48:56 -0800

    Enemy wave spawner.

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -19,7 +19,7 @@ public class Enemy_Movement : MonoBehaviour
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if(Vector3.Distance(transform.position, target.position) <= 0.1f){
+        if(Vector3.Distance(transform.position, target.position) <= 0.5f){
             
             GetNextWaypoint();
         }
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform spawnPos;
+    public Text waveCountdownTimerText;
+
+    private float waitTime = 10f;
+    private float countdown = 5f;
+    private int waveIndex = 0;
+
+    private void Update(){
+
+        if(countdown <= 0f){
+
+            StartCoroutine(SpawnEnemyWave());
+            countdown = waitTime;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownTimerText.text = Mathf.Round(countdown).ToString();
+    }
+
+    private IEnumerator SpawnEnemyWave(){
+
+        waveIndex++;
+
+        for(int i = 0; i < waveIndex; i++){
+
+            SpawnEnemy();
+            yield return new WaitForSeconds(1f);
+
+        }
+    }
+
+    private void SpawnEnemy(){
+        Instantiate(enemyPrefab, spawnPos.position, Quaternion.identity);
+    }
+}

6c4c0e61b2de8ae330850cdf87f4e1da95c2b679
Mel Rodrigues <melrodriguesgames@gmail.com>
2021-01-19 21:08:10 -0800

    Basic build manager.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    #region SINGLETON PATTERN
+    public static BuildManager instance;
+
+    private void Awake(){
+        if (instance !=  null){
+            Destroy(instance);
+            instance = this;
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion 
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild(){
+        return turretToBuild;
+    }
+
+    private void Start(){
+        turretToBuild = standardTurretPrefab;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private Renderer nodeRenderer;
+    private Color startColor;
+    private GameObject turret;
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Vector3 offset;
+
+    private void Start(){
+        nodeRenderer = GetComponent<Renderer>();
+        startColor = nodeRenderer.material.color;
+    }
+
+    private void OnMouseEnter() {
+        nodeRenderer.material.color = hoverColor;
+    }
+
+    private void OnMouseExit() {
+        nodeRenderer.material.color = startColor;
+    }
+
+    private void OnMouseDown() {
+        if(turret != null){
+            Debug.Log("CAN'T BUILD HERE! WEAPON ALREADY IN PLACE!");
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject) Instantiate(turretToBuild, transform.position + offset, transform.rotation);        
+    }
+}
